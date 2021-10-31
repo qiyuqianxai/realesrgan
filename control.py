e39cf1993887f05cdfda8877e2ea73184e3f603c
qiyuqianxai <2358341499@qq.com>
2021-10-31 20:35:40 +0800

    1031

---
+++ control.py
@@ -0,0 +1,24 @@
+import os
+from time import sleep
+import json
+from multiprocessing import Process
+states_json_path = "/workspace/go_proj/src/Ai_WebServer/algorithm_utils/model_states.json"
+model_name = "realesrgan"
+
+def run_model_server():
+    os.system("python run.py")
+
+if __name__ == '__main__':
+    running = False
+    while True:
+        with open(states_json_path,"r",encoding="utf-8")as f:
+            states = json.load(f)
+            if states[model_name] and not running:
+                p = Process(target=run_model_server,args=())
+                p.start()
+                running = True
+            elif not states[model_name] and running:
+                os.system("ps -ef|grep python|grep -v control|cut -c 9-16|xargs kill -9")
+                running = False
+        sleep(1)
+        print("control...")
